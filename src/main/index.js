3fae525f83cfddf2b62435cf66ba5086d9b76731
Gavin McDermott <gavinmcdermott@gmail.com>
2018-08-28 16:48:30 -0700

    add initial handling for app minimize, maximize, close, and quit

--- src/main/index.js
+++ src/main/index.js
@@ -10,22 +10,53 @@ if (process.env.NODE_ENV !== 'development') {
   global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
 
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
-const winURL = process.env.NODE_ENV === 'development'
+
+// Variable to track if the user is closing the app or quitting it
+let willQuitApp = false
+
+
+const WINDOW_URL = process.env.NODE_ENV === 'development'
   ? `http://localhost:9080`
   : `file://${__dirname}/index.html`
 
-function createWindow () {
+// Handles the initialization of the application window
+const createWindow = async () => {
   /**
    * Initial window options
    */
   mainWindow = new BrowserWindow({
-    height: 563,
+    title: 'Smirk',
+    x: 50,
+    y: 50,
+    height: 777,
+    width: 1300,
     useContentSize: true,
-    width: 1000
+    show: false,
+    maximizable: false,
+    fullscreen: false,
+    fullscreenable: false
+    // resizable: false
   })
 
-  mainWindow.loadURL(winURL)
+  mainWindow.loadURL(WINDOW_URL)
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+  })
+
+  // 'close' fires before 'closed' event, this handler checks
+  // if the user wants to quit the app or just close the window
+  mainWindow.on('close', (event) => {
+    // the user is quitting the entire app
+    // the user only tried to close the window, not quit the app
+    if (!willQuitApp) {
+      event.preventDefault()
+      mainWindow.hide()
+    }
+  })
 
   mainWindow.on('closed', () => {
     mainWindow = null
@@ -41,9 +72,23 @@ app.on('window-all-closed', () => {
 })
 
 app.on('activate', () => {
-  if (mainWindow === null) {
+  const appNeedsWindow = mainWindow &&
+    !mainWindow.isVisible() &&
+    !mainWindow.isMinimized()
+
+  // Note on OS X: This re-creates the app's window when the dock icon is
+  // clicked and the window is not currently open
+  if (appNeedsWindow) {
     createWindow()
   }
+})
+
+// 'before-quit' is emitted when Electron receives the signal to quit the app
+// This is a different behavior than minimizing it or closing the app
+// When the user want to actually quit the app, we use this variable
+// to ensure that quit happens, not just a window close / minimization
+app.on('before-quit', () => {
+  willQuitApp = true
 })
 
 /**
