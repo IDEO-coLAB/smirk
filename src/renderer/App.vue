5d88c6d6ace1d7f42f004a47573cd8e988783da9
Gavin McDermott <gavinmcdermott@gmail.com>
2018-08-28 13:46:28 -0700

    initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'smirk'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
