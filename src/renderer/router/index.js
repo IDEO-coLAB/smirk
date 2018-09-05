68248080615928b90164a91fac677b944a613c3b
Gavin McDermott <gavinmcdermott@gmail.com>
2018-09-05 14:20:51 -0700

    add TransactionPage with associated route and components to app

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -1,20 +1,39 @@
+import _ from 'lodash'
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import TransactionsPage from '@/pages/TransactionsPage'
+import TransactionsPage from '../pages/TransactionsPage'
+import TransactionPage from '../pages/TransactionPage'
+
+import { WALLET_SERVICE_MUTATIONS } from '../store/modules/AppState'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/transactions',
       name: 'transactions-page',
-      component: TransactionsPage
+      component: TransactionsPage,
+      beforeEnter: (to, from, next) => {
+        // appState housekeeping:
+        // clear out the app's currentTransaction when arriving here
+        const $store = this.a.app.$store
+        if (_.isNil($store)) {
+          return next()
+        }
+        $store.commit(WALLET_SERVICE_MUTATIONS.SET_CURRENT_TX, null)
+        next()
+      }
+    },
+    {
+      path: '/transaction/:id',
+      name: 'transaction-page',
+      component: TransactionPage
     },
     {
       path: '*',
-      redirect: '/'
+      redirect: '/transactions'
     }
   ]
 })
